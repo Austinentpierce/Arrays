5ae9403a5a5b882109580b285fa9e4a622432f52
Austin Pierce <Austinentpierce@gmail.com>
2021-08-05 14:32:00 -0400

    Arrays finished

--- Program.cs
+++ Program.cs
@@ -2,11 +2,20 @@
 
 namespace Arrays
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            int[] luckyNumbers = { 7, 4, 5, 10, 9, 15 };
+            string[] friends = new string[5];
+            friends[0] = "Jim";
+            friends[1] = "Kelly";
+            luckyNumbers[1] = 900;
+
+
+            Console.WriteLine(luckyNumbers[1]);
+
+            Console.ReadLine();
+        }
     }
-  }
 }
